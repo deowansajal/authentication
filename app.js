afa88ba6878f818ab19ffe360350c0b3f59de67f
mdsajaldeown <mdshajol659@gmail.com>
2019-10-15 02:14:45 +0600

    delete package.json.lock and node_modules and install

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use("/api/users", userRouters);
 
 if(process.env.NODE_ENV === "production"){
 	app.use(express.static("client/build"))
+	app.get("*", (req,res)=> {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	})
 }
 
 // Connect to the mongoDb with mongoose
